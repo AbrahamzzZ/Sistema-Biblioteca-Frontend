892f555ab5cfd5c4afa62dac2c867a72412fcfb1
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-12-22 21:15:36 -0500

    Organizando las rutas del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,56 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './module/components/layouts/auth-layout/auth-layout';
+import { Main } from './module/components/layouts/main-layout/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
+        component: AuthLayout,
+        children: [
+            {
+                path: 'login',
+                loadChildren: () =>
+                    import('./module/pages/auth/auth-module').then(
+                        (m) => m.AuthModule
+                    ),
+            },
+            {
+                path: '**',
+                redirectTo: 'login',
+                pathMatch:'full'
+            }
+        ]
+    },
+    {
+        path: 'home',
+        component: Main,
+        children: [
+            {
+                path: 'loan',
+                loadChildren: () => 
+                    import('./module/pages/book-loan/book-loan-module').then(
+                        (m) => m.BookLoanModule
+                    )
+            },
+            {
+                path: 'client',
+                loadChildren: () =>
+                    import('./module/pages/user/user-module').then(
+                        (m) => m.UserModule
+                    )
+            }
+        ]
+    },
+    {
+        path: '**',
+        loadChildren: () =>
+            import('./module/pages/page-not-found/page-not-found-module').then(
+                (m) => m.PageNotFoundModule
+            )
+    }
+];
