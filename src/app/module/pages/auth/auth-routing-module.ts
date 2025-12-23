1e3f480e7eaf4098155e591b00da2409c5cba3de
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-12-22 21:19:58 -0500

    Se creo el modulo Auth

---
+++ src/app/module/pages/auth/auth-routing-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+
+const routes: Routes = [
+  {
+    path: '', component: Login, title: 'Sistema Bibliotecario'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
