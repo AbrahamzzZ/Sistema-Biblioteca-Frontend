f26b925a96f088a0b2e44b80dc4d0692bdb338cf
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-01-17 10:32:52 -0500

    Se creo la interfaz de modulo Book

---
+++ src/app/core/interfaces/book.ts
@@ -0,0 +1,12 @@
+export interface Book{
+    id: number;
+    codigo: string;
+    titulo: string;
+    autor: string;
+    editorial: string;
+    anioPublicacion: number;
+    genero: string;
+    stock: number;
+    ubicacion: string;
+    estado: boolean;
+}
