562b50d6036a0a05d845b2c3d5bb2ab0cca10600
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-02-10 23:12:35 -0500

    Se creo un componente compartido tipo text area

---
+++ src/app/shared/components/text-area/text-area.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from '../../ui/material-module';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-area',
+  imports: [MaterialModule, ReactiveFormsModule],
+  templateUrl: './text-area.html',
+  styleUrl: './text-area.css',
+})
+export class TextArea {
+  @Input() label!: string;
+  @Input() control!: FormControl;
+
+  @Input() placeholder = '';
+  @Input() rows = 3;
+  @Input() maxlength?: number;
+}
